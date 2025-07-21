55746c95d528223413c4f7517495d129d1f1320a
Francis <franciskarani15@gmail.com>
2025-07-21 23:01:50 +0300

    adds input fields to the form

--- src/pages/RabbitForm.jsx
+++ src/pages/RabbitForm.jsx
@@ -5,6 +5,26 @@ export default function RabbitForm() {
   return (
     <div className='formDisplay' >
     <h1>Home</h1>
+    <form> 
+        <label> Rabbit name </label>
+        <input 
+        id=''
+        type='text'
+        name=''
+        placeholder='Enter Rabbit name'
+        // value={name}
+        required
+        
+        />
+
+        <label>Enter the Gender</label>
+        <select id='' 
+        // value={gender}
+        >
+            <option>Male</option>
+            <option>female</option>
+        </select>
+    </form>
       
     </div>
   )
