dcf4a07e1baa4b2e36fefe840fab7a3fdf350420
derrick <derrick.mwangi2@student.moringaschool.com>
2025-07-22 22:29:51 +0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,57 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AnimalList from './pages/AnimalList';
+import RabbitForm from './pages/RabbitForm';
+import './App.css';
 import { useState } from 'react'
-import './App.css'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import Footer from './components/Footer.jsx';
+import Login from './pages/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
+   // state variable abstraction
+  const [formData, setFormData]= useState({
+    name:'',
+    gender:'',
+    served:'false',
+    dateServed:'',
+    probableDateOfBirth:''
+  })
 
+  // function to handle change of the input fields
+
+  function handleChange(e) {
+    const {name, value, type, checked} = e.target;
+    setFormData((prev)=>({
+      ...prev,
+      [name]:type==='checkbox'?checked:value
+    }))
+    
+  }
   return (
-    <>
-      
+    <Router>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rabbits" element={<AnimalList />} />
+        <Route 
+          path="/rabbitform" 
+          element={<RabbitForm formData={formData} handleChange={handleChange}/>}
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Footer />
-    </>
-  )
+    </Router>
+  );
 }
 
 export default App;
