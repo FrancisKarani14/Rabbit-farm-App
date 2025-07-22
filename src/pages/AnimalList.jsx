51e9f25e3f0d1c436b89ccf2eaff795d2b704e92
Francis <franciskarani15@gmail.com>
2025-07-22 22:05:53 +0300

    adds an image url in the db.json

--- src/pages/AnimalList.jsx
+++ src/pages/AnimalList.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-export default function AnimalList() {
+export default function AnimalList({rabbits}) {
+  // function to create a card for rabbits
+  function createCard() {
+    
+    
+  }
   return (
     <div>
       
