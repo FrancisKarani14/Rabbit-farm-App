513412bd3d8b6a8fff089d230a1adf3aeed4b547
Francis <franciskarani15@gmail.com>
2025-07-22 21:43:30 +0300

    adds a use effect to fetch the number of animals there sre in the farm

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,14 @@ import Home from './pages/Home';
 import AnimalList from './pages/AnimalList';
 import RabbitForm from './pages/RabbitForm';
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 function App() {
+
+  //variable to save the server base url
+  const url = " http://localhost:3000/rabbits" 
+
    // state variable abstraction
   const [formData, setFormData]= useState({
     name:'',
@@ -18,6 +22,20 @@ function App() {
     dateServed:'',
     probableDateOfBirth:''
   })
+  // a state variable for the animals, to record the changes in the number and the details
+  const[rabbits, setRabbits] = useState([])
+
+  // useeffect to fetch the animal lists as the page renders,
+  
+  useEffect(()=>{
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>setRabbits(data))
+    .catch(err => console.error("Fetch Error:", err));
+
+    
+    
+  },[])
 
   // function to handle change of the input fields
 
