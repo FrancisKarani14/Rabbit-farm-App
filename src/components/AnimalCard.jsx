f27ec4a0ca010beaee2b01ec727eb8409304b36a
Francis <franciskarani15@gmail.com>
2025-07-22 22:58:19 +0300

    creates a card for the animals

--- src/components/AnimalCard.jsx
+++ src/components/AnimalCard.jsx
@@ -3,6 +3,16 @@ import React from 'react'
 export default function AnimalCard({rabbit}) {
   return (
     <div className='animalCard'>
+        <h2> <strong>Name:</strong> {rabbit.name} </h2>
+        <img 
+        src='{rabbit.image} '
+        alt='{rabbit.name} '
+
+        />
+        <p><strong>Gender:</strong> {rabbit.gender} </p>
+         <p><strong>Is the rabbit served:</strong> {rabbit.served} </p>
+          <p><strong>Date when the rabbit was served:</strong> {rabbit.dateServed} </p>
+           <p><strong>Probable date of birth:</strong> {rabbit.probableBirthDate} </p>
 
       
     </div>
