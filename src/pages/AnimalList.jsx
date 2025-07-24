71bc7fb5cd7315965aca7bc013885665655d60c8
Francis <franciskarani15@gmail.com>
2025-07-24 08:22:48 +0300

    creates a SearchBar and passes a rabbits props

--- src/pages/AnimalList.jsx
+++ src/pages/AnimalList.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import AnimalCard from '../components/AnimalCard'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import SearchBar from '../components/SearchBar'
 
 
 export default function AnimalList({rabbits}) {
  return (
   <>
   <Navbar />
- 
+  <SearchBar rabbits={rabbits} />
     <div className='listDisplay'>
       {rabbits.map((rabbit)=>(
         <AnimalCard key={rabbit.id} rabbit={rabbit} />
