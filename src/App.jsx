cb8651370fb5b1148f015b605d2bc6a6fa27e676
Francis <franciskarani15@gmail.com>
2025-07-24 15:03:16 +0300

    completes merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,176 +1,97 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import AnimalList from './pages/AnimalList';
-import { useState, useEffect } from 'react';
-import RabbitForm from './pages/RabbitForm';
-import './App.css';
-
-import Signup from './pages/Signup';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-import Login from "./pages/Loginpage";
-
-
-
 function App() {
+  const url = "https://json-server-7-kr3u.onrender.com/rabbits";
 
-  //variable to save the server base url
-  const url = "https://json-server-7-kr3u.onrender.com/rabbits" 
+  const [formData, setFormData] = useState({
+    name: '',
+    image: '',
+    gender: '',
+    served: false,
+    dateServed: '',
+    probableBirthDate: ''
+  });
 
-   // state variable abstraction
-  const [formData, setFormData]= useState({
-    name:'',
-    image:'',
-    gender:'',
-    served:false,
-    dateServed:'',
-    probableBirthDate:''
-  })
-  // a state variable for the animals, to record the changes in the number and the details
-  const[rabbits, setRabbits] = useState([]);
+  const [rabbits, setRabbits] = useState([]);
 
-  // useeffect to fetch the animal lists as the page renders,
-  
-  useEffect(()=>{
+  useEffect(() => {
     fetch(url)
-    .then(res=>res.json())
-    .then(data=>setRabbits(data))
-    .catch(err => console.error("Fetch Error:", err));
-
-    
-    
-  },[])
-
-
-  // function to handle change of the input fields
+      .then(res => res.json())
+      .then(data => setRabbits(data))
+      .catch(err => console.error("Fetch Error:", err));
+  }, []);
 
   function handleChange(e) {
-    const {name, value, type, checked} = e.target;
-    setFormData((prev)=>({
+    const { name, value, type, checked } = e.target;
+    setFormData(prev => ({
       ...prev,
-      [name]:type==='checkbox'?checked:value
-    }))
-    
+      [name]: type === 'checkbox' ? checked : value
+    }));
   }
-  // function to handle submit
+
   function handleSubmit(e) {
-    e.preventDefault()
-    fetch(url,{
-      method:"POST",
-      headers:{
-        "Content-Type":"application/json",
+    e.preventDefault();
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-      body:JSON.stringify(formData)
-     })
-      .then(res=>res.json())
-      .then(newRabbit=>{
-        setRabbits(prev=> [...prev, newRabbit])
-      })
-.catch((err) => console.error("Post Error:", err));
-
- setFormData({
-        name: '',
-        image: '',
-        gender: '',
-        served: false,
-        dateServed: '',
-        probableBirthDate: ''
-      });
-    
-
-
-    }
-function handleDelete(id) {
-  console.log("Deleting rabbit with id:", id); 
-
-  fetch(`https://json-server-7-kr3u.onrender.com/rabbits/${id}`, {
-    method: "DELETE",
-  })
-    .then(() => {
-      console.log("Deleted on server");
-
-     
-      setRabbits((prevRabbits) =>
-        prevRabbits.filter((rabbit) => rabbit.id !== id)
-      );
+      body: JSON.stringify(formData)
     })
-    .catch((err) => {
-      console.error("Delete error:", err);
-    });
-}
-   }
+      .then(res => res.json())
+      .then(newRabbit => {
+        setRabbits(prev => [...prev, newRabbit]);
+        setFormData({
+          name: '',
+          image: '',
+          gender: '',
+          served: false,
+          dateServed: '',
+          probableBirthDate: ''
+        });
+      })
+      .catch((err) => console.error("Post Error:", err));
+  }
 
-  // function to handle update
-   
+  function handleDelete(id) {
+    fetch(`${url}/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        setRabbits(prev => prev.filter(rabbit => rabbit.id !== id));
+      })
+      .catch(err => console.error("Delete Error:", err));
+  }
+
   const handleUpdate = (updatedRabbit) => {
-   fetch(`https://json-server-7-kr3u.onrender.com/rabbits/${updatedRabbit.id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(updatedRabbit),
-  })
-    .then((res) => res.json())
-    .then((newRabbit) => {
-      const updatedList = rabbits.map((rabbit) =>
-        rabbit.id === newRabbit.id ? newRabbit : rabbit
-      );
-      setRabbits(updatedList);
-    });
-};
-
+    fetch(`${url}/${updatedRabbit.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedRabbit),
+    })
+      .then(res => res.json())
+      .then(newRabbit => {
+        const updatedList = rabbits.map(rabbit =>
+          rabbit.id === newRabbit.id ? newRabbit : rabbit
+        );
+        setRabbits(updatedList);
+      });
+  };
 
   return (
-         <>
     <Router>
- 
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/home" element={<Home />}/>
-
-        <Route path="/animallist" element={<AnimalList  rabbits={rabbits} handleDelete={handleDelete} onUpdate={handleUpdate}/>} />
-        <Route path="/rabbitform" element={<RabbitForm formData={formData} handleChange={handleChange}handleSubmit={handleSubmit}  />} />
-
-       
-        <Route 
-          path="/rabbitform" 
-          element={<RabbitForm formData={formData} handleChange={handleChange}handleSubmit={handleSubmit} />}
-        />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/animallist" element={
+          <AnimalList rabbits={rabbits} handleDelete={handleDelete} onUpdate={handleUpdate} />
+        } />
+        <Route path="/rabbitform" element={
+          <RabbitForm formData={formData} handleChange={handleChange} handleSubmit={handleSubmit} />
+        } />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        </Routes>
-    
+      </Routes>
     </Router>
-    </>
   );
 }
- 
-export default App;
-
-
-
-
-
- 
-
-
-
- 
-
-
-
-       
-
-        
-      
-
-   
-
- 
-          
-     
-
-
